abb64ba5e17b7622acb976b89d2ce5e98e1f299e
seungchanHan <seungchanhan00@gmail.com>
2022-02-14 00:49:56 +0900

    섹션 1 문제 8 정답

---
+++ section1/8_일곱난쟁이/solution.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+  const sum = arr.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let faker = arr[i] + arr[j];
+
+      if (sum - faker === 100) {
+        arr.splice(i, 1);
+        arr.splice(j, 1);
+
+        return arr;
+      }
+    }
+  }
+}
+console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]));
