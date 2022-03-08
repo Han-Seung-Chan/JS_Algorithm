2142df6dbb7143f8f7cd3635295a1067225a5635
seungchanHan <seungchanhan00@gmail.com>
2022-03-09 00:38:16 +0900

    섹션 3 문제 1 출력결과

---
+++ section3/2_유효한_팰린드롬/solution.js
@@ -0,0 +1,26 @@
+function solution(str) {
+  const strArr = str.split('');
+  const reverse = str.split('').reverse();
+  let answer = true;
+  const forward = [];
+  const opposite = [];
+
+  strArr.forEach((el) => {
+    let ASCII = el.toLowerCase().charCodeAt();
+    if (ASCII >= 97 && ASCII <= 122) forward.push(el.toLowerCase());
+  });
+
+  reverse.forEach((el) => {
+    let ASCII = el.toLowerCase().charCodeAt();
+    if (ASCII >= 97 && ASCII <= 122) opposite.push(el.toLowerCase());
+  });
+
+  forward.forEach((el, i) => {
+    if (opposite[i] !== el) {
+      answer = false;
+    }
+  });
+
+  return answer;
+}
+console.log(solution('found7, time: study; Yduts; emit, 7Dnuof'));
