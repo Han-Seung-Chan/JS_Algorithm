5ac65e3ccda51b47033aff735293802de999a63d
seungchanHan <seungchanhan00@gmail.com>
2022-04-01 20:44:17 +0900

    섹션 4 문제 2 정답

--- section4/2_뒤집은_소수/solution.js
+++ section4/2_뒤집은_소수/solution.js
@@ -1,6 +1,6 @@
 function solution(arr) {
   let answer;
-  let reversedNumberArray = [];
+  let reversedNumArr = [];
   arr.forEach((item) => {
     let reversedText = '';
     item
@@ -8,17 +8,20 @@ function solution(arr) {
       .split('')
       .reverse()
       .forEach((item) => (reversedText += item));
-    reversedNumberArray.push(Number(reversedText));
+
+    reversedNumArr.push(Number(reversedText));
   });
-  console.log(reversedNumberArray);
-  answer = reversedNumberArray.filter((item) => {
+
+  answer = reversedNumArr.filter((item) => {
     if (item === 1) return false;
-    console.log(Math.sqrt(item));
+
     for (let i = 2; i <= Math.sqrt(item); i++) {
       if (item % i === 0) return false;
     }
+
     return true;
   });
+
   return answer;
 }
 console.log(solution([32, 55, 62, 20, 250, 370, 200, 30, 100]));
