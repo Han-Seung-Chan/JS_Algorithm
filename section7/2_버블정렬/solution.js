1076f55b3cbca2e152c6a4eea51f33d998f58684
seungchanHan <seungchanhan00@gmail.com>
2022-05-06 19:02:02 +0900

    섹션 7 문제 2 정답

---
+++ section7/2_버블정렬/solution.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+  for (let i = arr.length - 1; i > 0 - 1; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
+
+/* 버블 정렬 
+
+버블 정렬은 첫 번째 자료와 두 번째 자료를, 두 번째 자료와 세 번째 자료를, 세 번째와 네 번째를, … 이런 식으로 (마지막-1)번째 자료와 마지막 자료를 비교하여 교환하면서 자료를 정렬한다
+*/
